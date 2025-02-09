4f6371099a3cf7b27f10a4c97e0e6c19c9410444
chevipo <ch0556734@gmail.com>
2025-02-09 20:38:20 +0200

    add environment

--- src/service.js
+++ src/service.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = "http://localhost:5294"
-axios.defaults.baseURL = apiUrl;
+axios.defaults.baseURL = process.env.REACT_APP_apiUrl;
 
 axios.interceptors.response.use(
   response => {
@@ -18,7 +17,7 @@ axios.interceptors.response.use(
 export default {
   getTasks: async () => {
     try{
-      const result = await axios.get(`${apiUrl}/items`)    
+      const result = await axios.get(`${process.env.REACT_APP_apiUrl}/items`)    
       return result.data;
     }catch (error) {
       console.error('Failed to fetch tasks:', error);
